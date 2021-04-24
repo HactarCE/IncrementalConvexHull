82dd79e6a0bf6f04ca67b7843085dc0144c82a67
Neill Robson <neill@neillrobson.com>
2021-04-24 16:14:40 -0400

    Remove algorithm.py

--- graph.py
+++ graph.py
@@ -1,9 +1,8 @@
 from __future__ import annotations
-from algorithm import flip_between
 import itertools
 
 import operator
-from typing import Iterable, List, Sequence, TypeVar, Union
+from typing import Iterable, List, Sequence, Tuple, TypeVar, Union
 
 import numpy as np
 import point
@@ -267,3 +266,24 @@ def pairwise(iterable: Iterable[T]):
     a, b = itertools.tee(iterable)
     next(b, None)
     return zip(a, b)
+
+
+def flip_between(g: Graph, ai: int, bi: int):
+    """Transform the given graph's triangulation such that an edge between a and b exists.
+
+    ai and bi are the indices of the vertices in graph g to connect.
+    """
+    # When looking "across" the hull from a to b, the vertices in the right
+    # slice are on the right-hand side from the perspective of a.
+    right_slice: Sequence[Vertex] = g[ai+1:bi]
+    left_slice: Sequence[Vertex] = g[bi+1:ai]
+
+    cross_edges: List[Tuple[Vertex, Vertex]] = []
+
+    for rv in right_slice:
+        for lv in reversed(left_slice):
+            if rv.nbrs.contains(lv):
+                cross_edges.append((rv, lv))
+
+    for c in cross_edges:
+        g.flip_edge(*c)
