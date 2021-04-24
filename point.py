27a9a72d53142dd484991af692d381a0f90e386e
HactarCE <6060305+HactarCE@users.noreply.github.com>
2021-04-23 22:56:39 -0400

    Document functions in point.py

--- point.py
+++ point.py
@@ -2,12 +2,17 @@ import numpy as np
 
 
 def dist(p, q):
+    """Returns the Euclidean distance between two points."""
     return (sum((p-q)**2))**0.5
 
 
-def orient(*points):
-    """Compute the orientation of three points."""
-    d = np.linalg.det([np.append(p, 1) for p in points])
+def orient(p1, p2, p3):
+    """Compute the orientation of three points.
+
+    Returns +1 if the points are in counterclockwise order, -1 if the points are
+    in clockwise order, or 0 if the points are colinear.
+    """
+    d = np.linalg.det([np.append(p, 1) for p in [p1, p2, p3]])
     if d > 0:
         return 1
     elif d < 0:
