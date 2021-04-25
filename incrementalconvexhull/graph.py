b0c603cda7df3adbfc25108b68bfca365377fb25
Neill Robson <neill@neillrobson.com>
2021-04-25 17:50:11 -0400

    Start testing vertex_pairs()

--- incrementalconvexhull/graph.py
+++ incrementalconvexhull/graph.py
@@ -42,7 +42,7 @@ class Graph:
             ai = self.index(a)
             bi = self.index(b)
 
-            flip_between(self, ai, bi)
+            self.flip_between(ai, bi)
 
             for v in self[ai+1:bi]:
                 self.remove_vertex(v)
@@ -186,6 +186,25 @@ class Graph:
         for i in range(len(self)):
             yield (self[i], self[i+1])
 
+    def flip_between(self, ai: int, bi: int):
+        """Transform the given graph's triangulation such that an edge between a and b exists.
+        ai and bi are the indices of the vertices in graph g to connect.
+        """
+        # When looking "across" the hull from a to b, the vertices in the right
+        # slice are on the right-hand side from the perspective of a.
+        right_slice: Sequence[Vertex] = self[ai+1:bi]
+        left_slice: Sequence[Vertex] = self[bi+1:ai]
+
+        cross_edges: List[Tuple[Vertex, Vertex]] = []
+
+        for rv in right_slice:
+            for lv in reversed(left_slice):
+                if rv.nbrs.contains(lv):
+                    cross_edges.append((rv, lv))
+
+        for c in cross_edges:
+            self.flip_edge(*c)
+
 
 class Vertex:
     """Vertex in an undirected graph of 2D Euclidean points.
@@ -240,23 +259,3 @@ class Vertex:
         n = len(self.nbrs)
         for i in range(n):
             yield (self.nbrs[i], self.nbrs[(i+1) % n])
-
-
-def flip_between(g: Graph, ai: int, bi: int):
-    """Transform the given graph's triangulation such that an edge between a and b exists.
-    ai and bi are the indices of the vertices in graph g to connect.
-    """
-    # When looking "across" the hull from a to b, the vertices in the right
-    # slice are on the right-hand side from the perspective of a.
-    right_slice: Sequence[Vertex] = g[ai+1:bi]
-    left_slice: Sequence[Vertex] = g[bi+1:ai]
-
-    cross_edges: List[Tuple[Vertex, Vertex]] = []
-
-    for rv in right_slice:
-        for lv in reversed(left_slice):
-            if rv.nbrs.contains(lv):
-                cross_edges.append((rv, lv))
-
-    for c in cross_edges:
-        g.flip_edge(*c)
--- incrementalconvexhull/test_graph.py
+++ incrementalconvexhull/test_graph.py
@@ -1,6 +1,7 @@
 import unittest
 from . import graph
 
+
 class PolygonTest(unittest.TestCase):
     def test_add_vertex(self):
         pass
@@ -10,3 +11,14 @@ class PolygonTest(unittest.TestCase):
 
     def test_find_convex_nbrs(self):
         pass
+
+    def test_vertex_pairs(self):
+        g = graph.Graph()
+        v0 = g.add_vertex(0, 0)
+        v1 = g.add_vertex(0, 1)
+        v2 = g.add_vertex(1, 0)
+        v3 = g.add_vertex(1, 1)
+        should_return = [(v0, v2), (v2, v3), (v3, v1), (v1, v0)]
+        for pair in g.vertex_pairs():
+            should_return.remove(pair)
+        self.assertEqual(0, len(should_return))
